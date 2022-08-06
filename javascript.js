18542cf71ceda02648948af7928e626cf3b14e44
rakuzo <rakuzoxilion@gmail.com>
2022-08-06 13:54:06 +0700

    Add multiple box

---
+++ javascript.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('#container');
+
+
+for (let i = 0; i < 256; i++) {
+    const div = document.createElement('div');
+    div.classList.add('inactive');
+    container.appendChild(div);
+}
