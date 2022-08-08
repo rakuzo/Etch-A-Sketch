e432b5dbe7a21ae49a518af713c465c3dcbf5d58
rakuzo <rakuzoxilion@gmail.com>
2022-08-08 11:19:59 +0700

    Implement customize box number button

--- javascript.js
+++ javascript.js
@@ -1,22 +1,27 @@
 const container = document.querySelector('#container');
 
-
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.setAttribute('id','box');
-    div.classList.add('inactive');
-    container.appendChild(div);
+function createBoxes () {
+    let boxNumber = prompt('Input Number:');
+    let double = boxNumber * boxNumber;
+    for (let i = 0; i < double; i++) {
+        const div = document.createElement('div');
+        div.setAttribute('id','box');
+        div.classList.add('inactive');
+        container.appendChild(div);
+    }
+    const boxes = document.querySelectorAll('#box');
+    for (let box of boxes) {
+        draw(box, (e) => {
+            e.target.classList.remove('inactive');
+            e.target.classList.add('active');
+            });
+    }
 }
 
 function draw(element, enter) {
     element.addEventListener('mouseenter', enter);
 }
 
-const boxes = document.querySelectorAll('#box');
 
-for (let box of boxes) {
-    draw(box, (e) => {
-        e.target.classList.remove('inactive');
-        e.target.classList.add('active');
-        });
-}
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', createBoxes)
